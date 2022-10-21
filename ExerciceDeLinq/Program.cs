d56031b15accc342580c5395ed071d6ba141e9da
Samuel Legrain <samuel.legrain@bstorm.be>
2022-10-21 08:49:06 +0200

    Ajoutez des fichiers projet.

---
+++ ExerciceDeLinq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using LINQDataContext;
+
+namespace ExerciceDeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataContext dc = new DataContext();            
+
+            #region Console.ReadLine()
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
